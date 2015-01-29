a446684f4e3c6a6702fc72ba8070a7a51ddfed0f
KlingbergJE <john_klingberg@yahoo.com>
2015-01-29 16:18:24 -0600

    small fixes

--- WriteFile.cpp
+++ WriteFile.cpp
@@ -3,31 +3,29 @@
 
 Write::Write(const char* file_name)
 {
-   Write* w = new Write;
-   w->output_file.open(file_name);
-   w->closed = false;
-   return w;
+   output_file.open(file_name);
+   closed = false;
 }
 
 Write::~Write() //calls the desctructor from the class in the header file to get rid of the file created
 {
-   close(w);
-   delete w;
+   close();
+   delete [];
 }
 
 void Write:: close() //closes out the file so that no new information can be added to it
 {
-   if (!w->closed)
+   if (!closed)
    {
-      w->output_file.close();
-      w->closed = true;
+      output_file.close();
+      closed = true;
    }
 }
 
 void Write:: writeLine(String* line)//allows the file to be written in and the data within edited
 {
-   if (!w->closed && line->length() > 0)//makes sure that the file is open for editing and there is room to make changes
+   if (!closed && line->length() > 0)//makes sure that the file is open for editing and there is room to make changes
    {
-      w->output_file << line->getText() << endl; // pulls the changes from the input and adds them to the file
+      output_file << line->getText() << endl; // pulls the changes from the input and adds them to the file
    }
 }
